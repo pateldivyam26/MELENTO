14342a5e00643040de46b7637deca837956fd047
pateldivyam26 <79200448+pateldivyam26@users.noreply.github.com>
2024-07-10 21:07:26 +0530

    Added mat table

--- MELENTO_Client/src/app/components/view-course/view-course.component.ts
+++ MELENTO_Client/src/app/components/view-course/view-course.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Course } from '../../models/course';
 import { CourseService } from '../../services/course.service';
 import { Category } from '../../models/category';
 import { CategoryService } from '../../services/category.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-view-course',
@@ -10,18 +13,41 @@ import { CategoryService } from '../../services/category.service';
   styleUrl: './view-course.component.scss'
 })
 export class ViewCourseComponent {
-  arrCourse:Course[]=[]
-  arrCategory: Category[] = [];
+  displayedColumns: string[] = ['id', 'cName', 'cDescription', 'category'];
+  dataSource = new MatTableDataSource<Course>();
+
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   categoryMap: { [id: number]: string } = {};
-  constructor(private courseService:CourseService, private categoryService: CategoryService){
-    this.courseService.getCourses().subscribe(data=>{
-      this.arrCourse=data
-    })
-    this.categoryService.getCategory().subscribe((data) => {
-      this.arrCategory = data;
-      this.arrCategory.forEach(category => {
-        this.categoryMap[category.id] = category.categoryName;
-      });
+
+  constructor(private courseService: CourseService, private categoryService: CategoryService) { }
+
+  ngOnInit(): void {
+    this.loadCourses();
+    this.loadCategories();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  loadCourses(): void {
+    this.courseService.getCourses().subscribe((data: Course[]) => {
+      this.dataSource.data = data.map(course => ({
+        ...course,
+        category: this.categoryMap[course.categoryId] || 'Unknown'
+      }));
+    });
+  }
+
+  loadCategories(): void {
+    this.categoryService.getCategory().subscribe((data: Category[]) => {
+      this.categoryMap = data.reduce((map, category) => {
+        map[category.id] = category.categoryName;
+        return map;
+      }, {} as { [id: number]: string });
     });
   }
 }
